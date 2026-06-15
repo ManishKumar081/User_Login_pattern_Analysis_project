5bebcc1ef4802544ca1a1acd5f5f8083efe4d38d
bhavik89389 <ssbhavik2006@gmail.com>
2026-06-15 19:34:33 +0530

    Update App.jsx

--- stock-in-ui/App.jsx
+++ stock-in-ui/App.jsx
@@ -1,1 +1,19 @@
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import StockInPage from "./components/StockInPage";
 
+function App() {
+  return (
+    <div className="app-layout">
+      <Sidebar />
+
+      <div className="main-area">
+        <Navbar />
+        <StockInPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
