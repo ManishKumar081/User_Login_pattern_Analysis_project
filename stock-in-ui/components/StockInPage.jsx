98c681e4ad5e434190cdb7e3e96faeee13d88acc
bhavik89389 <ssbhavik2006@gmail.com>
2026-06-15 19:37:23 +0530

    Update StockInPage.jsx

--- stock-in-ui/components/StockInPage.jsx
+++ stock-in-ui/components/StockInPage.jsx
@@ -1,1 +1,22 @@
+import { Box, Paper, Typography } from "@mui/material";
 
+export default function StockInPage() {
+  return (
+    <Box p={3}>
+      <Paper
+        sx={{
+          p: 3,
+          borderRadius: 3,
+        }}
+      >
+        <Typography variant="h5">
+          Entry Details
+        </Typography>
+
+        <Typography sx={{ mt: 2 }}>
+          Stock In Page Layout 
+        </Typography>
+      </Paper>
+    </Box>
+  );
+}
