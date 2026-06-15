421fab7db2e9675ccb74446800834c36b6cb787b
bhavik89389 <ssbhavik2006@gmail.com>
2026-06-15 19:36:38 +0530

    Update Navbar.jsx

--- stock-in-ui/components/Navbar.jsx
+++ stock-in-ui/components/Navbar.jsx
@@ -1,1 +1,25 @@
+import { Box, Typography } from "@mui/material";
 
+export default function Navbar() {
+  return (
+    <Box
+      sx={{
+        background: "white",
+        px: 4,
+        py: 2,
+        borderBottom: "1px solid #e5e7eb",
+      }}
+    >
+      <Typography
+        variant="h5"
+        fontWeight="bold"
+      >
+        Material Entry
+      </Typography>
+
+      <Typography color="gray">
+        Stock &gt; Material Entry
+      </Typography>
+    </Box>
+  );
+}
